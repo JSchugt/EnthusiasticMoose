2f8f7871fb8f08ec0ea71603f4d4659c64cdda7c
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-05-21 13:05:52 -0500

    Moose asks questions

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+void MooseSays(string message)
+{
+    Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+}
+Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
+MooseSays("H I, I'M  E N T H U S I A S T I C !");
+MooseSays("I really am enthusiastic");
